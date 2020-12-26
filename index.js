44694fe7d1e81b297e044e6e0f4b50c8bce18f51
WandyRosa <wandy.s.scheffer.r@gmail.com>
2020-12-26 01:43:15 -0300

    add fix of empty fields

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ function calculateRelatedFunction() {
     const x2 = parseInt(document.getElementById("x2").value);
     const y2 = parseInt(document.getElementById("y2").value);
 
+    if (isNaN(x1) || isNaN(y1) || isNaN(x2) || isNaN(y2)) {
+        alert("You should fill all fields...");
+        return;
+    }
+
 
     if (x1 == x2) {
         alert("It's not a related function...");
@@ -43,17 +48,19 @@ function calculateRelatedFunction() {
 
 function calculateAnotherPoint(typeOfCordenate) {
     if (a == null || b == null) {
-        alert("You may define a related function first...");
+        alert("You should define a related function first...");
         return;
     }
     
     if (typeOfCordenate) {
         const yi = parseInt(document.getElementById("yi").value);
+        if(isNaN(yi)){alert("you should fill the current field"); return;}
         x = (yi - b) / a;
         var content = `(${x}, ${yi})`;
         document.getElementById("point_with_y").innerHTML=content;
     }else{
         const xi = parseInt(document.getElementById("xi").value);
+        if(isNaN(xi)){alert("you should fill the current field"); return;}
         y = a*xi + b;
         var content = `(${xi}, ${y})`;
         document.getElementById("point_with_x").innerHTML=content;
